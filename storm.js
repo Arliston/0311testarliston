c031d1f25f4a9bb35838da93a4eac7ac4775002d
Arliston <145625942+Arliston@users.noreply.github.com>
2025-11-19 23:08:50 -0500

    Update storm.js: i just had to hit CTRL F5 god dammit

--- storm.js
+++ storm.js
@@ -285,7 +285,8 @@ class Storm{
                         tracks.stroke(col);
                         let pos = adv.pos;
                         let nextPos = this.record[this.record.length-1].pos;
-                        if(simSettings.trackMode===1 || (t>=this.formationTime && (!this.dissipationTime || t<this.dissipationTime))) tracks.line(pos.x,pos.y,nextPos.x,nextPos.y);                                              
+                        if(simSettings.trackMode===1 || (t>=this.formationTime && (!this.dissipationTime || t<this.dissipationTime))) tracks.line(pos.x,pos.y,nextPos.x,nextPos.y);          
+                        if(simSettings.trackMode===1 || (t>=this.formationTime && (!this.dissipationTime || t<this.dissipationTime))) tracks.circle(pos.x,pos.y,6,nextPos.x,nextPos.y);                              
                     }
                 }else if(this.aliveAt(viewTick) || simSettings.trackMode===2 || selectedStorm===this){
                     for(let n=0;n<this.record.length-1;n++){
@@ -299,7 +300,8 @@ class Storm{
                         tracks.stroke(col);
                         let pos = adv.pos;
                         let nextPos = this.record[n+1].pos;
-                        tracks.line(pos.x,pos.y,nextPos.x,nextPos.y);                                               
+                        tracks.line(pos.x,pos.y,nextPos.x,nextPos.y);    
+                        tracks.circle(pos.x,pos.y,6,nextPos.x,nextPos.y);                            
                     }
                 }
             }
